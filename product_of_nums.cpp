f68a9c570a2786587710f9db83a09cf6d28b68b3
Ananthula Varshitha <83571478+20wh1a05f2@users.noreply.github.com>
2021-11-28 08:59:25 +0530

    product of two numbers

---
+++ product_of_nums.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float n1,n2,product;
+	cout<<"Enter two numbers: ";
+	cin>>n1;
+	cin>>n2;
+	product = n1*n2;
+	cout<<"Product = "<<product; 
+	
+}
